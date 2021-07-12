a6d9db86f4189f54fb0626c72896501dc2ac5a5b
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2021-07-12 19:45:57 +0300

    Minor Change in Icon and Variable

--- components/icons/IconSettings.js
+++ components/icons/IconSettings.js
@@ -2,3 +2,5 @@ export const grid = { color: '#fff', size: '16' }
 export const search = { color: '#fff', size: '24' }
 export const enlarge = { color: '#6F8099', size: '24' }
 export const shrink = { color: '#081C81', size: '24' }
+export const charge = { color: '#fff', size: '16' }
+export const apple = { size: '25' }
