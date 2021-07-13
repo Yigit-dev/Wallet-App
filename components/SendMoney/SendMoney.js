d13026f8288a38aee3d910f464cf9ef760e51306
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2021-07-13 20:29:51 +0300

    SendMoney Component Added

---
+++ components/SendMoney/SendMoney.js
@@ -0,0 +1,34 @@
+import { AddButton } from '../Buttons/Buttons'
+import Person from '../Person/Person'
+import Slider from '../Slider/Slider'
+import { TextMedium } from '../Typography/Typography'
+import style from './SendMoney.module.css'
+
+const SendMoney = () => {
+  return (
+    <div className={style.SendMoney}>
+      <TextMedium className={style.title}>Send Money</TextMedium>
+      <div className={style.Slider}>
+        <Slider>
+          <li>
+            <AddButton />
+          </li>
+          <li>
+            <Person name="Chris" />
+          </li>
+          <li>
+            <Person name="Yiğit" />
+          </li>
+          <li>
+            <Person name="Şeftali" />
+          </li>
+          <li>
+            <Person name="Kavun" />
+          </li>
+        </Slider>
+      </div>
+    </div>
+  )
+}
+
+export default SendMoney
