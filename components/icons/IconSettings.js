7ebfb8f5a820ab8116a14c1745f8484f638ff7e6
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2021-07-13 20:15:38 +0300

    Icon X Edited

--- components/icons/IconSettings.js
+++ components/icons/IconSettings.js
@@ -3,4 +3,5 @@ export const search = { color: '#fff', size: '24' }
 export const enlarge = { color: '#6F8099', size: '24' }
 export const shrink = { color: '#081C81', size: '24' }
 export const charge = { color: '#fff', size: '16' }
+export const x = { color: '#fff', size: '30' }
 export const apple = { size: '25' }
--- components/icons/X.js
+++ components/icons/X.js
@@ -6,16 +6,22 @@ function SvgX(props) {
       width="1em"
       height="1em"
       fontSize={props.size}
-      viewBox="0 0 29 29"
+      viewBox="0 0 30 30"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
-        d="M21.213 14.142H7.071M14.142 7.071v14.142"
+        d="M22.0465 14.9755H7.9044"
         stroke={props.color}
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.9755 7.90442V22.0466"
+        stroke={props.color}
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   )
