b3f5f75a33e0ae4d76a006d10f93d95aa7b5b6e2
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2021-07-13 20:30:52 +0300

    Added SendMoney to Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import CardSlider from '../components/CardSlider/CardSlider'
 import Layout from '../components/Layout/Layout'
+import SendMoney from '../components/SendMoney/SendMoney'
 import TopBar from '../components/TopBar/TopBar'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <Layout>
       <TopBar />
       <CardSlider />
+      <SendMoney />
     </Layout>
   )
 }
