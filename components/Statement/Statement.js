9b1d020d769e81b495355988d22c6f27d42b576f
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2021-07-18 02:06:21 +0300

    Statement Component Added

--- components/Statement/Statement.js
+++ components/Statement/Statement.js
@@ -4,14 +4,16 @@ import { TextMediumShadow, TextBold } from '../Typography/Typography'
 const Statement = () => {
   return (
     <div className={style.Statement}>
-      <div className="income">
-        <TextMediumShadow>Income</TextMediumShadow>
-        <TextBold>$2 542.25</TextBold>
-      </div>
-      <h4>/</h4>
-      <div className="spent">
-        <TextMediumShadow>Spent</TextMediumShadow>
-        <TextBold>$1 832.74</TextBold>
+      <div className={style.content}>
+        <div className={style.income}>
+          <TextMediumShadow>Income</TextMediumShadow>
+          <TextBold>$2 542.25</TextBold>
+        </div>
+        <h4>/</h4>
+        <div className={style.spent}>
+          <TextMediumShadow>Spent</TextMediumShadow>
+          <TextBold>$1 832.74</TextBold>
+        </div>
       </div>
     </div>
   )
