b7c1345450149fa9a6867f8b87d46d97286b4309
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2021-07-17 22:37:08 +0300

    TopBar Added for Page

--- pages/card_detail.js
+++ pages/card_detail.js
@@ -1,11 +1,18 @@
 import Layout from '../components/Layout/Layout'
+import TotalBalance from '../components/TotalBalance/TotalBalance'
+import WaveBackground from '../components/WaveBackground/WaveBackground'
+import { LeftArrow } from '../components/icons'
+import { leftArrow } from '../components/icons/IconSettings'
+import TopBar from '../components/TopBar/TopBar'
 
 export default function CardDetailPage() {
   return (
     <Layout>
-      <div>
-        <h1>Card Detail</h1>
-      </div>
+      <WaveBackground />
+      <TopBar>
+        <LeftArrow color={leftArrow.color} size={leftArrow.size} />
+      </TopBar>
+      <TotalBalance />
     </Layout>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,16 @@ import History from '../components/History/History'
 import Layout from '../components/Layout/Layout'
 import SendMoney from '../components/SendMoney/SendMoney'
 import TopBar from '../components/TopBar/TopBar'
+import { Grid, Search } from '../components/icons'
+import { grid, search } from '../components/icons/IconSettings'
 
 export default function Home() {
   return (
     <Layout>
-      <TopBar />
+      <TopBar>
+        <Grid color={grid.color} size={grid.size} />
+        <Search color={search.color} size={search.size} />
+      </TopBar>
       <CreditCard />
       <SendMoney />
       <History />
