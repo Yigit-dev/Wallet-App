767549ef7169b4abea81b7be11de9d01ef38c16e
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2021-07-18 17:00:32 +0300

    Bug Fixed

--- components/icons/X.js
+++ components/icons/X.js
@@ -14,14 +14,14 @@ function SvgX(props) {
       <path
         d="M22.0465 14.9755H7.9044"
         stroke={props.color}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M14.9755 7.90442V22.0466"
         stroke={props.color}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
